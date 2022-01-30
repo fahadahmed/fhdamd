59a73e9f2ce2d03666da9530d36a0fb1f19c3ba1
Fahad Ahmed <fahad.ahmed@me.com>
2022-01-30 18:48:35 +1100

    FHD-1 readjusting the component files

--- packages/components/lib/components.esm.js
+++
@@ -1,2 +0,0 @@
-module.exports=function(){console.log("Front-end components package")};
-//# sourceMappingURL=components.esm.js.map
--- packages/components/lib/components.umd.js
+++
@@ -1,2 +0,0 @@
-!function(n){"function"==typeof define&&define.amd?define(n):n()}(function(){module.exports=function(){console.log("Front-end components package")}});
-//# sourceMappingURL=components.umd.js.map
