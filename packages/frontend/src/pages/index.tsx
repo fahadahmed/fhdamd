71fc31a4fe4fa68bad2c64e7def93174f215088f
Fahad Ahmed <fahad.ahmed@me.com>
2022-02-05 17:51:04 +1100

    FHD-1 setting up global styles

---
+++ packages/frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppContainer } from '../styles/common.styles';
+
+function IndexPage() {
+  return (
+    <AppContainer>
+      <h1>Hello React</h1>
+    </AppContainer>
+  )
+}
+
+export default IndexPage;
---
+++ packages/frontend/src/styles/common.styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const AppContainer = styled.div`
+  display: grid;
+  padding: 16px;
+  background: #efefef;
+  height: 100vh;
+`;
