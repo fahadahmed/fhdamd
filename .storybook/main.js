06cd9b169fec29374823ce2bc334687bfbb7a753
Fahad Ahmed <fahad.ahmed@me.com>
2022-12-27 22:52:18 +1100

    moved the components folder out of app folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    '../app/components/**/*.stories.mdx',
-    '../app/components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
