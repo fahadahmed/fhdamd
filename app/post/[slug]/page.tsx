ab23e213f86aae16b1773dbdcba43e329f9be561
Fahad Ahmed <fahad.ahmed@me.com>
2022-12-28 22:51:43 +1100

    added code to show tags in the post

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { API_URL } from '../../../utils/constants';
 
-async function getData(slug) {
+async function getData(slug: string) {
   const res = await fetch(`${API_URL}/api/post/${slug}`, {
     cache: 'no-store'
   });
   return await res.json();
 }
-export default async function Index({ params: { slug } }) {
+
+type Props = {
+  params: {
+    slug: string;
+  }
+}
+export default async function Index({ params: { slug } }: Props) {
   const post = await getData(slug);
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
+      <div>
+        <h4>Tags</h4>
+        {post.tags.map(tag => <span key={tag}>{tag} </span>)}
+      </div>
     </div>
   )
 }
