a2d8199f339ed691f0b0bb35f8683d106af8a9bf
Fahad Ahmed <fahad.ahmed@me.com>
2023-01-04 13:58:45 +1100

    created a new create post route

---
+++ app/admin/posts/create/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CreatePost() {
+  return (
+    <div>
+      <h1>Create a New Post</h1>
+      <p>Add a form that creates a new post with markdown support here.</p>
+    </div>
+  )
+}
--- app/admin/posts/page.tsx
+++ app/admin/posts/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
+import { API_URL } from '../../../utils/constants';
+import type { Post } from '../../../pages/types/post';
+
+async function getAllPosts() {
+  const res = await fetch(`${API_URL}/api/posts`, { cache: 'no-store' });
+  return res.json();
+}
 
 export default async function Posts() {
-
+  const posts = await getAllPosts();
   return (
     <div>
       <h3>Posts Section</h3>
@@ -11,6 +19,14 @@ export default async function Posts() {
         <li>Update Posts</li>
         <li>Delete Posts</li>
       </ul>
+      <div><Link href='admin/posts/create'>Create a new Post</Link></div>
+      <div>
+        {posts.map((post: Post) => (
+          <div key={post.id}>
+            <Link href={`admin/post/${post.slug}`}>{post.title}</Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
