3b63ba148eb1a608db0195d04c6d50d4dba4a298
Fahad Ahmed <fahad.ahmed@me.com>
2023-01-04 11:53:44 +1100

    updated the code for signing out properly

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -38,29 +38,19 @@ const SIDEBAR_ITEMS = [
   }
 ]
 
-export default async function RootLayout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
   const data = await getUserSession();
-  console.log(data.user);
   if (!data.user) {
     redirect('/login')
   }
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>
-        <div style={{ display: 'grid', gridTemplateColumns: '3fr 9fr', gap: '1rem' }}>
-          <SideNav navLinks={SIDEBAR_ITEMS} />
-          <div>{children}</div>
-        </div>
-      </body>
-    </html>
+    <div style={{ display: 'grid', gridTemplateColumns: '3fr 9fr', gap: '1rem' }}>
+      <SideNav navLinks={SIDEBAR_ITEMS} />
+      <div>{children}</div>
+    </div>
   )
 }
--- components/LogoutButton/LogoutButton.tsx
+++ components/LogoutButton/LogoutButton.tsx
@@ -1,14 +1,22 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { API_URL } from '../../utils/constants';
+import { redirect } from 'next/navigation';
 
 export default function LogoutButton() {
+  const [loggedOut, setLoggedOut] = useState(false);
+
   const handleLogout = async () => {
-    console.log('we must handle the logout functionality here');
     const res = await fetch(`${API_URL}/api/auth/signOutUserSession`, {
       cache: 'no-store'
     });
-    console.log(res.status)
+    if (res.status === 200) {
+      setLoggedOut(true)
+    }
+  }
+
+  if (loggedOut) {
+    redirect('/login')
   }
   return (
     <button onClick={handleLogout}>Logout</button>
