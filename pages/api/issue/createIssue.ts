9aaf251ce13474775f3b0cd17a2edd889576d485
Fahad Ahmed <fahad.ahmed@me.com>
2023-01-15 18:23:35 +1100

    fahadahmed/roadmap-personal#16 implemented the add new issue route and api

---
+++ pages/api/issue/createIssue.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import admin from '../../../lib/firebaseAdmin';
+
+type Data = {
+  tag: string;
+};
+
+const db = admin.firestore();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const name = req.body.name;
+  const description = req.body.description;
+
+  try {
+    const response = await db.collection('issues').add({ name, description });
+    console.log(response.id);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+
+  res.status(200).json({ tag: name });
+}
