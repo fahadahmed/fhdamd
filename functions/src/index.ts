a75e56adfa3096e27b86dbc071164ff7cd7812b1
Fahad Ahmed <fahad.ahmed@me.com>
2023-06-27 17:37:32 +1000

    added firebase folder to gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
-import * as functions from "firebase-functions";
+/* eslint-disable object-curly-spacing */
+import * as functions from 'firebase-functions';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
