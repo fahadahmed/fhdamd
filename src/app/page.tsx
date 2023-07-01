b435ac407c0ba4fd3dd7154c41bc96b01693b9c7
Fahad Ahmed <fahad.ahmed@me.com>
2023-07-01 20:23:55 +1000

    adding sample data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { css } from '../../styled-system/css'
 
 const heading = css({
@@ -7,10 +6,35 @@ const heading = css({
   fontFamily: 'Inter, sans-serif',
 })
 
-export default function Home() {
+async function getData() {
+  const res = [
+    {
+      id: 1,
+      title: 'Integrating Firebase with Remix apps'
+    },
+    {
+      id: 2,
+      title: 'Building data-driven dynamic forms with Remix, remix-validated-form & zod'
+    },
+    {
+      id: 3,
+      title: 'How to implement styles using Panda CSS in Remix apps'
+    }
+  ]
+  return res;
+}
+
+export default async function Home() {
+  const posts = await getData();
+  console.log(posts)
   return (
     <main>
       <div className={heading}>Hello 🐼!</div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
     </main>
   )
 }
